0310c0c6e2d1454aeaacb9f5270413627ddec216
Lisa Heffernan <lisaheffernan@outlook.com>
2022-06-09 10:39:58 +0100

    refactor of 7ky kata and test

--- 7kyuKatas.js
+++ 7kyuKatas.js
@@ -6,3 +6,53 @@ function ballCollector(detritus) {
     monkeys.weight = 58 * tennisBalls.length;
     return monkeys;
   }
+
+  function countDevelopers(list) {
+    let counter = 0;
+    for (i = 0; i<list.length; i++){
+      if(list[i].continent === 'Europe' && list[i].language === 'JavaScript')
+        {
+          counter++;
+        }
+    }
+    return counter
+  }
+
+  function XO(str) {
+    let strX = str.match(/[x]/ig);
+    let strO = str.match(/[o]/ig);
+    if (strX === null && strO === null)
+    {
+      return true
+    }
+    else if (strX === null || strO === null)
+    {
+      return false
+    }
+    else if (strO.length === strX.length)
+    {
+      return true
+    }
+    else 
+    {
+      return false
+    }      
+  }
+
+  var splitInParts = function(s, partLength){
+    let array = s.split('');
+    let counter = 0;
+    for (let i = 1; i<(s.length/partLength); i++)
+      {
+      counter = partLength * i + i -1
+      array.splice(counter, 0, ' ');
+      }
+    return array.join('');
+  }
+
+  module.exports = {
+    ballCollector,
+    countDevelopers,
+    XO,
+    splitInParts
+  }
--- __tests__/7kyuKatas.test.js
+++ __tests__/7kyuKatas.test.js
@@ -1,6 +1,42 @@
-describe("Test Cases", function() {
-    Test.assertSimilar(ballCollector([58, 68, 62, 69, 58]), { weight: 116 }, "Should return the combined weight of just the tennis balls!");
-    Test.assertSimilar(ballCollector([60,66,71,68,62]), { weight: 0 }, "Should return the combined weight of just the tennis balls!");
-    Test.assertSimilar(ballCollector([58]), { weight: 58 }, "Should return the combined weight of just the tennis balls!");
-    Test.assertSimilar(ballCollector([58, 58, 58]), { weight: 174 }, "Should return the combined weight of just the tennis balls!");
+const { it, expect } = require('@jest/globals');
+const {
+    ballCollector,
+    countDevelopers,
+    XO,
+    splitInParts
+  } = require('../7kyuKatas');
+
+describe("ballCollector", () =>{
+    it('Returns the total weight of the tennis balls', () => {
+        expect(ballCollector([58, 68, 62, 69, 58])).toEqual({ weight: 116 });
+        expect(ballCollector([60,66,71,68,62])).toEqual({ weight: 0 });
+      });
+  });
+
+
+//   const assert = require('chai').assert;
+  describe("Sample tests", () => {
+      it("should find JavaScript developers from Europe", () => {
+    const list1 = [
+      { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+      { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+      { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+      { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+    ];
+    expect(countDevelopers(list1)).toEqual(1);
+    });
+  });
+
+describe("XO", () =>{
+    it('Returns true if same numbers of Xs as there are Os, otherwise returns false', () => {
+        expect(XO("xxOo")).toEqual(true);
+        expect(XO("xxxm")).toEqual(false);
+      });
+  });
+
+  describe("splitInParts", () => {
+    it("should split string into parts of length partLength", () => {
+    expect(splitInParts("supercalifragilisticexpialidocious", 3)).toEqual("sup erc ali fra gil ist ice xpi ali doc iou s")
+    expect(splitInParts("HelloKata", 1)).toEqual("H e l l o K a t a")
+    });
   });
