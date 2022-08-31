42d77656d9df5d5b505fd46583e68d93a11e917a
Lisa Heffernan <lisaheffernan@outlook.com>
2022-08-31 14:54:48 +0100

    practising git

--- 7kyuKatas.js
+++ 7kyuKatas.js
@@ -1,7 +1,7 @@
 function ballCollector(detritus) {
     let monkeys = {
       weight: 0
-    };
+    }
     const tennisBalls = detritus.filter(w => w === 58);
     monkeys.weight = 58 * tennisBalls.length;
     return monkeys;
